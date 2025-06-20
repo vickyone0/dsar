1fbec37de7da478032b9f28202af14b1e70f9ef2
vickyone0 <vigneshs0.618@gmail.com>
2025-06-20 04:58:10 -0400

    multi-thread

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,32 @@
 use dsar::linkedlist::Node;
 use dsar::binarytree::BinaryTree;
 
+use std::sync::{Arc, Mutex};
+use std::thread;
+
 fn main() {
+
+    let counter = Arc::new(Mutex::new(0));
+    let mut handles = vec![];
+
+
+    for _ in 0..10 {
+        let counter = Arc::clone(&counter);
+
+        let handle = thread::spawn(move || {
+            let mut num = counter.lock().unwrap();
+            *num += 1;
+        });
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    println!("Result: {}", *counter.lock().unwrap());
+
+
 
     let mut tree = BinaryTree::new();
     tree.insert(10);
